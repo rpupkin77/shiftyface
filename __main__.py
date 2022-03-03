0f902a373c0538123fbf4b28e8753b18b7d74caf
Paul Thompson <pt@sevenelm.com>
2022-03-02 23:17:41 -0500

    First commit, code functioning

---
+++ __main__.py
@@ -0,0 +1,6 @@
+from face_generator import FaceGenerator
+
+if __name__ == "__main__":
+
+    face_generator = FaceGenerator(100)
+    face_generator.generate()
---
+++ settings.py
@@ -0,0 +1,8 @@
+# NOTE THESE ARE DEFAULTS
+
+TRAITS_SETTINGS = {
+    "base_directory": "traits",
+    "traits_file": "traits.json",
+    "assets_directory": "assets",
+    "output_directory": "output"
+}
